9315e7cfb28ebdfe201f8a1af3350ce77d2824f8
MiiTHeRsZ <gustavo_leme05@hotmail.com>
2024-07-20 20:12:05 -0300

    Extracted Avatar component and removed duplicated CSS styles

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from '@phosphor-icons/react';
+import { Avatar } from './Avatar';
 import styles from './Comment.module.css'
 
 export function Comment() {
     return (
         <div className={styles.comment}>
-            <img src="https://github.com/miithersz.png" />
+            <Avatar hasBorder={false} src="https://github.com/miithersz.png" />
 
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
